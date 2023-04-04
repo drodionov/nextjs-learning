e3a5abeff88407beb5ebc1f23a847024cb06ea8b
Dmitrii Rodionov <drodionov.work@gmail.com>
2023-04-04 22:54:12 +0300

    Add MeetupDetails page and component with styling

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,12 @@
+import MeetupDetails from "../../components/meetups/MeetupDetails";
+
+const MeetupDetailsPage = () => {
+  return <MeetupDetails
+      image={'https://upload.wikimedia.org/wikipedia/commons/1/16/Skyscrapers_in_Izmir_-_Turkey.jpg'}
+      title={'Dummy Meetup'}
+      address={'Some Address, Building 5, 36500'}
+      description={'Some long long long long long long long long long long long long long description about this wonderful meetup'}/>
+
+}
+
+export default MeetupDetailsPage
